191d71310bf829b264e3c963258978076ba33c2a
Gideon <2101121@talnet.nl>
2021-04-09 00:48:23 +0200

    camping village opdracht

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>CampingVillage</title>
+    
+<body>
+
+<div class="topnav">
+  <a href="index.php"><img src="afbeelding2.png" width="170" height="100"></a>
+  <a href="activiteiten.php">ACTIVITEITEN</a>
+  <a href="reserveren.php">RESERVEREN</a>
+  <a href="contact.php">CONTACT</a>
+  <a href="admin.php">ADMIN</a>
+</div>
+
+
+</body>
+</html>
+
